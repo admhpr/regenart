5504f6883cfe6c79d8f36e31994cf2e808c294a4
adam <adam@harpur.io>
2019-08-27 21:37:32 +0100

    feat: added lerp

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 const canvasSketch = require("canvas-sketch");
+const { lerp } = require("canvas-sketch-util/math");
 
 const settings = {
   dimensions: "A4",
@@ -22,14 +23,14 @@ const sketch = () => {
     return points;
   }
   const points = createGrid();
-
+  const margin = 100;
   // render function
   return ({ context, width, height }) => {
     context.fillStyle = "white";
 
     points.forEach(([u, v]) => {
-      const x = u * width;
-      const y = v * height;
+      const x = lerp(margin, width - margin, u);
+      const y = lerp(margin, height - margin, v);
 
       context.beginPath();
       context.arc(x, y, 100, 0, Math.PI * 2, false);
