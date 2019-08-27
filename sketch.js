84cf957285e271f16397034c2827bc4600e666aa
adam <adam@harpur.io>
2019-08-27 21:33:10 +0100

    feat: simple grid

--- sketch.js
+++ sketch.js
@@ -6,17 +6,37 @@ const settings = {
 };
 
 const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = "blue";
-    context.fillRect(0, 0, width, height);
+  function createUV(coord, count) {
+    return count <= 1 ? 0.5 : coord / (count - 1);
+  }
+  function createGrid() {
+    const points = [];
+    const count = 5;
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = createUV(x, count);
+        const v = createUV(y, count);
+        points.push([u, v]);
+      }
+    }
+    return points;
+  }
+  const points = createGrid();
 
-    context.beginPath();
-    context.arc(width / 2, height / 2, width * 0.2, 0, Math.PI * 2, false);
-    context.fillStyle = "red";
-    context.fill();
-    context.lineWidth = width * 0.05;
-    context.strokeStyle = "yellow";
-    context.stroke();
+  // render function
+  return ({ context, width, height }) => {
+    context.fillStyle = "white";
+
+    points.forEach(([u, v]) => {
+      const x = u * width;
+      const y = v * height;
+
+      context.beginPath();
+      context.arc(x, y, 100, 0, Math.PI * 2, false);
+      context.storkeStyle = "black";
+      context.lineWidth = 40;
+      context.stroke();
+    });
   };
 };
 
